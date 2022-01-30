6b2d6320007ca51f56c3165688cd0c578b85b830
pchellaboina <praveen.chellaboina@gmail.com>
2022-01-30 17:43:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rojbjy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rojbjy)
---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+// src/app/my-grid-application/my-grid-application.component.ts
+import { Component } from '@angular/core';
+import { GridOptions } from 'ag-grid';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class MyGridApplicationComponent {
+  private gridOptions: GridOptions;
+
+  constructor() {
+    this.gridOptions = <GridOptions>{
+      enableSorting: true,
+      // enable filtering
+      enableFilter: true,
+    };
+    this.gridOptions.columnDefs = [
+      {
+        headerName: 'Asset Name',
+        minWidth: 150,
+        width: 250,
+        field: 'assetName',
+        cellRendererFramework: RouterLinkRendererComponent,
+        cellRendererParams: {
+          inRouterLink: '[/users/details/]',
+        },
+      },
+      {
+        headerName: 'Asset Id',
+        field: 'assetId',
+        width: 100,
+      },
+    ];
+    this.gridOptions.rowData = [
+      { assetId: 5, assetName: 'Five' },
+      { assetId: 10, assetName: 'Ten' },
+      { assetId: 15, assetName: 'Fifteen' },
+    ];
+  }
+}
